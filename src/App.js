e5f933aaa682c80652aa08eefdd3e63353136e98
naitikAIITS <159764477+naitikAIITS@users.noreply.github.com>
2024-02-20 11:48:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Html5QrcodeScanner } from 'html5-qrcode';
+
 
 function App() {
+
+    const[scanResult,setScanResult]=useState(null) 
+    useEffect(()=>{
+  
+      
+    const scanner=new Html5QrcodeScanner('reader',{
+      qrbox:{
+        width:250,
+        height:250,
+      },
+      fps:5,
+    })
+    scanner.render(success,error)
+  
+    function success(result){
+      scanner.clear()
+      setScanResult(result)
+  
+    }
+  
+    function error(err){
+      console.log(err);
+    }
+  
+  
+    },[])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h3>QR code scanner</h3>
+      {
+        scanResult
+        ?<div>Success:<a href={scanResult}>{scanResult}</a></div>
+        :<div id='reader'></div>
+      }
+
+      </div>
+    
   );
-}
+    }
 
 export default App;
